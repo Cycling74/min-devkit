8d42e7fae96c3497eb61a231dd0949ee9689e03a
Timothy Place <tim@cycling74.com>
2016-05-04 19:06:14 -0700

    banger: now uses a clock, has a new outlet

--- source/projects/banger/banger.cpp
+++ source/projects/banger/banger.cpp
@@ -1,5 +1,6 @@
 
 #include "c74_min.h"
+#include <random>
 
 using namespace c74::min;
 
@@ -7,9 +8,9 @@ using namespace c74::min;
 class banger : public object {
 public:
 
-	inlet	input	= { this, "(toggle) on/off" };
-	outlet	output	= { this, "(bang) triggers at randomized interval" };
-
+	inlet	input			= { this, "(toggle) on/off" };
+	outlet	bang_out		= { this, "(bang) triggers at randomized interval" };
+	outlet	interval_out	= { this, "(float) the interval for the current bang" };
 
 	banger(atoms args) {
 		if (args.size() > 0)
@@ -22,24 +23,66 @@ public:
 	~banger() {}
 
 
-    ATTRIBUTE (min, double, 1.0) {
-		if (args[0] < 1.0)
-			args[0] = 1.0;
-    }
-    END
-
-
-    ATTRIBUTE (max, double, 1.0) {
-		if (args[0] < 1.0)
-			args[0] = 1.0;
-    }
-    END
-
-
-	METHOD (bang) {
-		output.send("bang");
+	CLOCK (metro) {
+		auto interval = generate_random_number();
+		
+		interval_out.send(interval);
+		bang_out.send("bang");
+		
+		metro.delay(interval);
 	}
 	END
+
+
+    ATTRIBUTE (min, double, 250.0) {
+		double value = args[0];
+		
+		if (value < 1.0)
+			value = 1.0;
+		
+		args[0] = value;
+    }
+    END
+
+
+    ATTRIBUTE (max, double, 1500.0) {
+		double value = args[0];
+		
+		if (value < 1.0)
+			value = 1.0;
+		
+		args[0] = value;
+    }
+    END
+
+
+	ATTRIBUTE (on, bool, 0) {
+		if (args[0] == true)
+			metro.delay(0.0);	// fire the first one straight-away
+		else
+			metro.stop();
+	}
+	END
+	
+	
+	METHOD (toggle) {			// toggle method defines an "int" input but with special metadata
+		on = args[0];
+	}
+	END
+	
+
+private:
+	
+	/// code from http://en.cppreference.com/w/cpp/numeric/random/uniform_real_distribution
+	/// see also http://en.cppreference.com/w/cpp/numeric/random
+
+	double generate_random_number() {
+		std::random_device rd;
+		std::mt19937 gen(rd());
+		std::uniform_real_distribution<> dis(min, max);
+		return dis(gen);
+	}
+		
 };
 
 
