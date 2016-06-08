14523cb14b567537b3ec334664e7d0ddc045ab60
Benjamin Bracken <ben@cycling74.com>
2016-06-08 13:42:15 -0700

    random float generator example

---
+++ source/projects/randfloat/randfloat.cpp
@@ -0,0 +1,54 @@
+/// @file	
+///	@ingroup 	minexamples
+///	@copyright	Copyright (c) 2016, Cycling '74
+/// @author		Ben Bracken
+///	@license	Usage of this file and its contents is governed by the MIT License
+
+#include "c74_min.h"
+#include <random>
+
+using namespace c74::min;
+
+
+class randfloat : public object {
+public:
+
+	inlet	input	= { this, "(bang) generate random floating point number" };
+	outlet	output	= { this, "(float) random floating point number output" };
+
+	randfloat(atoms args) {
+		if (args.size() > 0)
+			min = args[0];
+		if (args.size() > 1)
+			max = args[1];
+	}
+
+
+	~randfloat() {}
+
+
+	METHOD (bang) {
+        std::random_device rd;
+        std::mt19937 mt(rd());
+        std::uniform_real_distribution<double> dist(min, max);
+        double value = dist(mt);
+		output.send(value);
+	}
+	END
+
+    /// Attributes are given a name, a type, a default value, and function to be called when setting the value
+    ATTRIBUTE (min, double, 0.0) {
+		double value = args[0];
+		args[0] = value;
+    }
+    END
+
+    ATTRIBUTE (max, double, 1.0) {
+		double value = args[0];
+		args[0] = value;
+    }
+    END
+};
+
+
+MIN_EXTERNAL(randfloat);
