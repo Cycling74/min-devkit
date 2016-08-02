b6e44fdf7132a276afbf795048a677326e8dd04d
Timothy Place <tim@cycling74.com>
2016-08-02 17:08:07 -0600

    initial hands-free doc generation

---
+++ docs/convolve.maxref.md
@@ -0,0 +1,28 @@
+# convolve~
+
+Perform convolution on a list.
+
+Convolution is...
+
+## Messages
+
+* `List` : Input to the convolution function.
+
+## Attributes
+
+* `kernel` : The convolution kernel.  
+             The kernel is the operand of the convolution function.
+             
+## Output
+
+A list of the same dimensions as the input list.
+
+## See Also
+
+* `buffir~` : Perform convolution on an audio signal using a buffer~ as the kernel.
+* `jit.convolve` : Convolve two matrices.
+
+---
+
+@Tags: Max Operators Lists  
+@Author: Cycling '74
--- source/projects/dcblocker_tilde/dcblocker_tilde.cpp
+++ source/projects/dcblocker_tilde/dcblocker_tilde.cpp
@@ -12,72 +12,66 @@ using namespace c74::min;
 class dcblocker : public object<dcblocker>, sample_operator<1,1> {
 public:
 	MIN_AUTHOR		{	"Cycling '74"		};
-	MIN_TAGS		{	"MSP", "Filters"	};
+	MIN_TAGS		{	"audio, filters"	};
 	MIN_DESCRIPTION {	"Filter out DC offset. "
 						"The DC offset or <a href='https://en.wikipedia.org/wiki/DC_bias'>DC bias</a>, is removed by applying a first-order highpass filter to the input."
-						""
+						"<br/><br/>"
 						"This first-order highpass filter algorithm is used pretty much everywhere (STK, ChucK, RTCMix, SuperCollider, Max, Pd, etc),"
 						"with the difference equation:"
-						""
+						"<br/><br/>"
 						"	y(n) = (1 * x(n))  +  (-1 * x(n-1))  -  (-0.9997 * y(n-1))  ,  n = 0, 1, 2, 3, ..."
-						""
+						"<br/><br/>"
 						"which can be simplified to:"
-						""
+						"<br/><br/>"
 						"	y(n) = x(n) - x(n-1) + (0.9997 * y(n-1))"
-						""
+						"<br/><br/>"
 						"and thus characterized by the Z-transform:"
-						""
+						"<br/><br/>"
 						"	Y(z) = X(z)  -  X(z) * z^(-1)  +  Y(z) * 0.9997 * z^(-1)"
-						""
+						"<br/><br/>"
 						"meaning the transfer function is:"
-						""
+						"<br/><br/>"
 						"	H(z) = [1  -  z^(-1)]  /  [1  +  0.9997 * z^(-1)]"
-						""
+						"<br/><br/>"
 						"and resulting in the frequency response:"
-						""
+						"<br/><br/>"
 						"	H( e^(i*omega*T) ) = [1  -  e^(-i * omega * T)]  /  [1  +  0.9997 * e^(-i * omega * T)]"
-						""
+						"<br/><br/>"
 						"where $i$ is the sqrt(-1), e is Euler's log base, T is the sampling interval, and omega is 2*pi*frequency."
-						""
+						"<br/><br/>"
 						"In Max, it usually shows up simply as [biquad~ 1.0 -1.0 0.0 -0.9997 0.0]."
 						"In other places it usually shows up with the feedback coefficient set to -0.995"
 						"(e.g in SuperCollider and in [JOS, 2007, pp 273])."
 						"The higher coefficient is desirable so as to not attenuate lowish frequencies in the spectrum,"
 						"but with the caveat that it also won't respond as quickly to varying amounts DC Offset."
-						""
+						"<br/><br/>"
 						"The power is attenuated by -3 dB at a normalized frequency of 0.1612 * pi @ 0.9997."
 						"At fs=44100 this translates to cf = 22050 * 0.1612 = 3554.46 Hz."
-						""
+						"<br/><br/>"
 						"The power is attenuated by -3 dB at a normalized frequency of 0.1604 * pi @ 0.995."
 						"At fs=44100 this translates to cf = 22050 * 0.1604 = 3536.82 Hz."
-						""
+						"<br/><br/>"
 						"For reference, in this last case, the power is attenuated by -6 db (magnitude attenuated by -12 dB) @ 0.0798 * pi,"
 						"which at fs=44100 translates to 1759.59 Hz."
-						""
 	};
 
 	inlet	input	{ this, "(signal) Input" };
 	outlet	output	{ this, "(signal) Output", "signal" };
-	
-	
-	///	Reset the DC-Blocking filter.
-	/// This algorithm uses an IIR filter, meaning that it relies on feedback.
-	/// If the filter should not be producing any signal (such as turning audio off and then back on in a host)
-	/// or if the feedback has become corrupted (such as might happen if a NaN is fed in)
-	/// then it may be neccesary to clear the filter by sending this message.
-	
-	message clear { this, "clear", MIN_FUNCTION {
-		x_1 = y_1 = 0.0;
-		return {};
-	}};
-	
-	
-	attribute<bool>	bypass { this, "bypass" , false };
+
+
+	message clear { this, "clear", "Reset the DC-Blocking filter. Because this is an IIR filter it has the potential to blow-up, requiring a reset.",
+		MIN_FUNCTION {
+			x_1 = y_1 = 0.0;
+			return {};
+		}
+	};
+
+
+	attribute<bool>	bypass { this, "bypass" , false, description{"Pass the input straight-through."} };
 
 
 	/// Process one sample
-	/// Note that we don't worry about denormal values in the feedback because
-	/// Max takes care of squashing them for us by setting the FTZ bit on the CPU.
+	/// Max takes care of squashing denormal for us by setting the FTZ bit on the CPU.
 
 	sample operator()(sample x) {
 		if (bypass)
@@ -89,12 +83,11 @@ public:
 			return y;
 		}
 	}
-	
+
 
 private:
 	sample	x_1 { 0.0 };	///< Input history
 	sample	y_1 { 0.0 };	///< Output history
 };
-
 
 MIN_EXTERNAL(dcblocker);
