a1cd3bb8bb98e1e84aff0d7558fdf9cf1f925425
Timothy Place <tim@cycling74.com>
2016-10-26 09:43:49 -0500

    min.edge~: updated to use the new built-in thread-checking and queueing mechanisms in the min api.

--- source/projects/min.edge_tilde/min.edge_tilde.cpp
+++ source/projects/min.edge_tilde/min.edge_tilde.cpp
@@ -11,37 +11,20 @@ using namespace c74::min;
 class edge : public object<edge>, sample_operator<1,0> {
 public:
 
-	inlet<>		input			{ this, "(signal) Input" };
-	outlet<>	output_true		{ this, "(bang) input is non-zero" };
-	outlet<>	output_false	{ this, "(bang) input is zero" };
-
-	timer deliverer { this, MIN_FUNCTION {
-		bool state;
-		while (transitions.try_dequeue(state)) {
-			if (state)
-				output_true.send("bang");
-			else
-				output_false.send("bang");
-		}
-		return {};
-	}};
+	inlet<>													input			{ this, "(signal) input" };
+	outlet<thread_check::scheduler, thread_action::fifo>	output_true		{ this, "(bang) input is non-zero" };
+	outlet<thread_check::scheduler, thread_action::fifo>	output_false	{ this, "(bang) input is zero" };
 
 	void operator()(sample x) {
-		if (x != 0.0 && prev == 0.0) { // change from zero to non-zero
-			transitions.try_enqueue(true);
-			deliverer.delay(0);
-		}
-		else if (x == 0.0 && prev != 0.0) { // change from non-zero to zero
-			transitions.try_enqueue(false);
-			deliverer.delay(0);
-		}
+		if (x != 0.0 && prev == 0.0)
+			output_true.send(k_sym_bang);	// change from zero to non-zero
+		else if (x == 0.0 && prev != 0.0)
+			output_false.send(k_sym_bang);	// change from non-zero to zero
 		prev = x;
 	}
 
 private:
 	sample		prev { 0.0 };
-	fifo<bool>	transitions { 100 };
 };
-
 
 MIN_EXTERNAL(edge);
