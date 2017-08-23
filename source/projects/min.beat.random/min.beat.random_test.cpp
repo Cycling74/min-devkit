afe47053471ba1b1732155ebde5439d4c40dd0e7
Timothy Place <tim@cycling74.com>
2017-08-23 09:31:02 -0500

    min.beat.random: fix for unit test crash which it turns out is caused by the weird way that the Catch macro blocks behave. see Cycling74/min-api#6.

--- source/projects/min.beat.random/min.beat.random_test.cpp
+++ source/projects/min.beat.random/min.beat.random_test.cpp
@@ -36,31 +36,31 @@ SCENARIO( "object produces correct output" ) {
 		REQUIRE( my_object.max == Approx(1500.0) );
 		
 		// now proceed to testing various sequences of events
-		WHEN( "the defaults are used" ) {
-			THEN( "nothing is produced by the object after waiting 5 seconds" ) {
-			
-				cout << "About to wait for 5 seconds..." << endl;
-				
-				// 1. Wait for 5 seconds
-				std::this_thread::sleep_for(5s);
-				
-				cout << "done!" << endl;
-				// 2. See if there was any output
-				auto& output = *c74::max::object_getoutput(my_object, 0);
-				REQUIRE( output.size() == 0 );
-			}
-			AND_THEN( "turning it on does produce output" ) {
-				
-				// 1. Wait for 5 seconds
-				std::this_thread::sleep_for(5s);
 
-				
-				// 2. See if there was any output
-				
-				
-				
-			}
-		}
+		INFO( "When the defaults are used nothing is produced by the object after waiting 5 seconds" )
+
+		// 1. Wait for 5 seconds
+		cout << "About to wait for 5 seconds..." << endl;
+		std::this_thread::sleep_for(5s);
+		cout << "done!" << endl;
+
+		// 2. See if there was any output
+		auto& output = *c74::max::object_getoutput(my_object, 0);
+		REQUIRE( output.size() == 0 );
+
+
+		INFO( "turning it on does produce output after waiting 5 seconds" )
+
+		my_object.on = true;
+
+		// 1. Wait for 5 seconds
+		cout << "About to wait for 5 seconds..." << endl;
+		std::this_thread::sleep_for(5s);
+		cout << "done!" << endl;
+
+		// 2. See if there was any output
+		output = *c74::max::object_getoutput(my_object, 0);
+		REQUIRE( output.size() > 0 );
 	}
 }
 
