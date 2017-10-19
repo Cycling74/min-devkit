47e39d4a9287ad4429406222242fefe2f9dab0d2
Timothy Place <tim@cycling74.com>
2017-10-19 12:30:40 -0500

    min.stress~: new object to load the cpu for stress testing patchers and externals.

---
+++ source/projects/min.stress_tilde/min.stress_tilde.cpp
@@ -0,0 +1,40 @@
+/// @file	
+///	@ingroup 	minexamples
+///	@copyright	Copyright (c) 2016, Cycling '74
+/// @author		Timothy Place
+///	@license	Usage of this file and its contents is governed by the MIT License
+
+#include "c74_min.h"
+
+using namespace c74::min;
+
+class stress : public object<stress>, public vector_operator<> {
+public:
+
+	MIN_DESCRIPTION { "Eat up a specified percentage of processor time to stress the computer." };
+	//MIN_TAGS		{ "analysis" };
+	MIN_AUTHOR		{ "Timothy Place, Rob Sussman" };
+	//MIN_RELATED		{ "edge~, snapshot~, ==~" };
+	
+	//inlet<>		input	{ this, "(signal) dummy" };
+
+
+	attribute<number, threadsafe::yes, limit::clamp> target { this, "target", 0.0,
+		range { 0.0, 100.0 },
+		description { "Percentage of the CPU to burn." }
+	};
+
+
+	void operator()(audio_bundle input, audio_bundle output) {
+		auto	svtime_ms	{ vector_size() / samplerate() * 1000.0 };
+		auto	spintime	{ svtime_ms * target / 100.0 };
+		auto	intime		{ c74::max::systimer_gettime() };
+		auto	outtime		{ intime + spintime };
+		size_t	spincount	{};
+
+		while (c74::max::systimer_gettime() < outtime)
+			++spincount;
+	}
+};
+
+MIN_EXTERNAL(stress);
