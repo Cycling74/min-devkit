5caffc54f65771c698bab34391003fb19813064a
Timothy Place <tim@cycling74.com>
2018-03-30 11:25:24 -0500

    Copyright updated to 2018

--- source/projects/min.note.make/min.note.make.cpp
+++ source/projects/min.note.make/min.note.make.cpp
@@ -1,8 +1,7 @@
-/// @file	
+/// @file
 ///	@ingroup 	minexamples
-///	@copyright	Copyright (c) 2016, Cycling '74
-/// @author		Timothy Place
-///	@license	Usage of this file and its contents is governed by the MIT License
+///	@copyright	Copyright 2018 The Min-DevKit Authors. All rights reserved.
+///	@license	Use of this source code is governed by the MIT License found in the License.md file.
 
 #include "c74_min.h"
 #include <queue>
