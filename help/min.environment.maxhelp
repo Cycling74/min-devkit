ac4cb33de0d7e4493a8d43349011ab7136260b23
Timothy Place <tim@cycling74.com>
2018-06-13 07:09:32 -0500

    min.environment: added MAC address outlet (implemented on the mac, windows forthcoming)

--- help/min.environment.maxhelp
+++ help/min.environment.maxhelp
@@ -9,7 +9,9 @@
 			"modernui" : 1
 		}
 ,
-		"rect" : [ 63.0, 104.0, 781.0, 433.0 ],
+		"rect" : [ 100.0, 100.0, 781.0, 433.0 ],
+		"bgcolor" : [ 0.898039, 0.898039, 0.898039, 1 ],
+		"editing_bgcolor" : [ 0.898039, 0.898039, 0.898039, 1 ],
 		"bglocked" : 0,
 		"openinpresentation" : 0,
 		"default_fontsize" : 12.0,
@@ -38,13 +40,27 @@
 		"subpatcher_template" : "tap.template",
 		"boxes" : [ 			{
 				"box" : 				{
+					"id" : "obj-1",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 570.0, 285.0, 150.0, 23.0 ],
+					"presentation_rect" : [ 570.0, 285.0, 150.0, 23.0 ],
+					"style" : "",
+					"text" : "60:03:08:92:28:ce"
+				}
+
+			}
+, 			{
+				"box" : 				{
 					"id" : "obj-10",
 					"maxclass" : "button",
 					"numinlets" : 1,
 					"numoutlets" : 1,
 					"outlettype" : [ "bang" ],
-					"patching_rect" : [ 180.0, 120.0, 24.0, 24.0 ],
-					"presentation_rect" : [ 180.0, 120.0, 24.0, 24.0 ],
+					"patching_rect" : [ 110.0, 120.0, 24.0, 24.0 ],
+					"presentation_rect" : [ 110.0, 120.0, 24.0, 24.0 ],
 					"style" : ""
 				}
 
@@ -52,13 +68,16 @@
 , 			{
 				"box" : 				{
 					"id" : "obj-8",
+					"linecount" : 2,
 					"maxclass" : "message",
 					"numinlets" : 2,
 					"numoutlets" : 1,
 					"outlettype" : [ "" ],
-					"patching_rect" : [ 485.0, 285.0, 150.0, 23.0 ],
-					"presentation_rect" : [ 485.0, 285.0, 150.0, 23.0 ],
-					"style" : ""
+					"patching_rect" : [ 415.0, 285.0, 150.0, 37.0 ],
+					"presentation_linecount" : 2,
+					"presentation_rect" : [ 415.0, 285.0, 150.0, 37.0 ],
+					"style" : "",
+					"text" : "\"Mac OS X Version 10.13.4 x86_64\""
 				}
 
 			}
@@ -69,9 +88,10 @@
 					"numinlets" : 2,
 					"numoutlets" : 1,
 					"outlettype" : [ "" ],
-					"patching_rect" : [ 325.0, 285.0, 150.0, 23.0 ],
-					"presentation_rect" : [ 325.0, 285.0, 150.0, 23.0 ],
-					"style" : ""
+					"patching_rect" : [ 255.0, 285.0, 150.0, 23.0 ],
+					"presentation_rect" : [ 255.0, 285.0, 150.0, 23.0 ],
+					"style" : "",
+					"text" : "x86_64"
 				}
 
 			}
@@ -82,9 +102,10 @@
 					"numinlets" : 2,
 					"numoutlets" : 1,
 					"outlettype" : [ "" ],
-					"patching_rect" : [ 170.0, 285.0, 150.0, 23.0 ],
-					"presentation_rect" : [ 170.0, 285.0, 150.0, 23.0 ],
-					"style" : ""
+					"patching_rect" : [ 100.0, 285.0, 150.0, 23.0 ],
+					"presentation_rect" : [ 100.0, 285.0, 150.0, 23.0 ],
+					"style" : "",
+					"text" : "mac"
 				}
 
 			}
@@ -93,10 +114,10 @@
 					"id" : "obj-2",
 					"maxclass" : "newobj",
 					"numinlets" : 1,
-					"numoutlets" : 3,
-					"outlettype" : [ "", "", "" ],
-					"patching_rect" : [ 215.0, 200.0, 95.0, 23.0 ],
-					"presentation_rect" : [ 215.0, 200.0, 95.0, 23.0 ],
+					"numoutlets" : 4,
+					"outlettype" : [ "", "", "", "" ],
+					"patching_rect" : [ 145.0, 200.0, 95.0, 23.0 ],
+					"presentation_rect" : [ 145.0, 200.0, 95.0, 23.0 ],
 					"style" : "",
 					"text" : "min.environment"
 				}
@@ -107,6 +128,13 @@
 				"patchline" : 				{
 					"destination" : [ "obj-2", 0 ],
 					"source" : [ "obj-10", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-1", 1 ],
+					"source" : [ "obj-2", 3 ]
 				}
 
 			}
@@ -151,9 +179,7 @@
 		"bgfillcolor_type" : "gradient",
 		"bgfillcolor_color1" : [ 0.376471, 0.384314, 0.4, 1.0 ],
 		"bgfillcolor_color2" : [ 0.290196, 0.309804, 0.301961, 1.0 ],
-		"bgfillcolor_color" : [ 0.290196, 0.309804, 0.301961, 1.0 ],
-		"bgfillcolor_angle" : 270.0,
-		"bgfillcolor_proportion" : 0.39
+		"bgfillcolor_color" : [ 0.290196, 0.309804, 0.301961, 1.0 ]
 	}
 
 }
--- source/projects/min.environment/min.environment.cpp
+++ source/projects/min.environment/min.environment.cpp
@@ -5,12 +5,145 @@
 
 #include "c74_min.h"
 #include <regex>
+
 #ifdef MAC_VERSION
 #include <Carbon/Carbon.h>
 #include <sys/utsname.h>
 
+#include <IOKit/IOKitLib.h>
+#include <IOKit/network/IOEthernetInterface.h>
+#include <IOKit/network/IONetworkInterface.h>
+#include <IOKit/network/IOEthernetController.h>
+#include <CoreFoundation/CoreFoundation.h>
+
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
+
+// Returns an iterator containing the primary (built-in) Ethernet interface. The caller is responsible for
+// releasing the iterator after the caller is done with it.
+static kern_return_t FindEthernetInterfaces(io_iterator_t* matchingServices) {
+	kern_return_t          kernResult;
+	CFMutableDictionaryRef matchingDict;
+	CFMutableDictionaryRef propertyMatchDict;
+
+	// Ethernet interfaces are instances of class kIOEthernetInterfaceClass.
+	// IOServiceMatching is a convenience function to create a dictionary with the key kIOProviderClassKey and
+	// the specified value.
+	matchingDict = IOServiceMatching(kIOEthernetInterfaceClass);
+
+	// Note that another option here would be:
+	// matchingDict = IOBSDMatching("en0");
+	// but en0: isn't necessarily the primary interface, especially on systems with multiple Ethernet ports.
+
+	if (NULL == matchingDict) {
+		printf("IOServiceMatching returned a NULL dictionary.\n");
+	}
+	else {
+		// Each IONetworkInterface object has a Boolean property with the key kIOPrimaryInterface. Only the
+		// primary (built-in) interface has this property set to TRUE.
+
+		// IOServiceGetMatchingServices uses the default matching criteria defined by IOService. This considers
+		// only the following properties plus any family-specific matching in this order of precedence
+		// (see IOService::passiveMatch):
+		//
+		// kIOProviderClassKey (IOServiceMatching)
+		// kIONameMatchKey (IOServiceNameMatching)
+		// kIOPropertyMatchKey
+		// kIOPathMatchKey
+		// kIOMatchedServiceCountKey
+		// family-specific matching
+		// kIOBSDNameKey (IOBSDNameMatching)
+		// kIOLocationMatchKey
+
+		// The IONetworkingFamily does not define any family-specific matching. This means that in
+		// order to have IOServiceGetMatchingServices consider the kIOPrimaryInterface property, we must
+		// add that property to a separate dictionary and then add that to our matching dictionary
+		// specifying kIOPropertyMatchKey.
+
+		propertyMatchDict
+			= CFDictionaryCreateMutable(kCFAllocatorDefault, 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+
+		if (NULL == propertyMatchDict) {
+			printf("CFDictionaryCreateMutable returned a NULL dictionary.\n");
+		}
+		else {
+			// Set the value in the dictionary of the property with the given key, or add the key
+			// to the dictionary if it doesn't exist. This call retains the value object passed in.
+			CFDictionarySetValue(propertyMatchDict, CFSTR(kIOPrimaryInterface), kCFBooleanTrue);
+
+			// Now add the dictionary containing the matching value for kIOPrimaryInterface to our main
+			// matching dictionary. This call will retain propertyMatchDict, so we can release our reference
+			// on propertyMatchDict after adding it to matchingDict.
+			CFDictionarySetValue(matchingDict, CFSTR(kIOPropertyMatchKey), propertyMatchDict);
+			CFRelease(propertyMatchDict);
+		}
+	}
+
+	// IOServiceGetMatchingServices retains the returned iterator, so release the iterator when we're done with it.
+	// IOServiceGetMatchingServices also consumes a reference on the matching dictionary so we don't need to release
+	// the dictionary explicitly.
+	kernResult = IOServiceGetMatchingServices(kIOMasterPortDefault, matchingDict, matchingServices);
+	if (KERN_SUCCESS != kernResult) {
+		printf("IOServiceGetMatchingServices returned 0x%08x\n", kernResult);
+	}
+
+	return kernResult;
+}
+
+
+// Given an iterator across a set of Ethernet interfaces, return the MAC address of the last one.
+// If no interfaces are found the MAC address is set to an empty string.
+// In this sample the iterator should contain just the primary interface.
+static kern_return_t GetMACAddress(io_iterator_t intfIterator, UInt8* MACAddress, UInt8 bufferSize) {
+	io_object_t   intfService;
+	io_object_t   controllerService;
+	kern_return_t kernResult = KERN_FAILURE;
+
+	// Make sure the caller provided enough buffer space. Protect against buffer overflow problems.
+	if (bufferSize < kIOEthernetAddressSize) {
+		return kernResult;
+	}
+
+	// Initialize the returned address
+	bzero(MACAddress, bufferSize);
+
+	// IOIteratorNext retains the returned object, so release it when we're done with it.
+	while ((intfService = IOIteratorNext(intfIterator))) {
+		CFTypeRef MACAddressAsCFData;
+
+		// IONetworkControllers can't be found directly by the IOServiceGetMatchingServices call,
+		// since they are hardware nubs and do not participate in driver matching. In other words,
+		// registerService() is never called on them. So we've found the IONetworkInterface and will
+		// get its parent controller by asking for it specifically.
+
+		// IORegistryEntryGetParentEntry retains the returned object, so release it when we're done with it.
+		kernResult = IORegistryEntryGetParentEntry(intfService, kIOServicePlane, &controllerService);
+
+		if (KERN_SUCCESS != kernResult) {
+			printf("IORegistryEntryGetParentEntry returned 0x%08x\n", kernResult);
+		}
+		else {
+			// Retrieve the MAC address property from the I/O Registry in the form of a CFData
+			MACAddressAsCFData = IORegistryEntryCreateCFProperty(controllerService, CFSTR(kIOMACAddress), kCFAllocatorDefault, 0);
+			if (MACAddressAsCFData) {
+				CFShow(MACAddressAsCFData);    // for display purposes only; output goes to stderr
+
+				// Get the raw bytes of the MAC address from the CFData
+				CFDataGetBytes((CFDataRef)MACAddressAsCFData, CFRangeMake(0, kIOEthernetAddressSize), MACAddress);
+				CFRelease(MACAddressAsCFData);
+			}
+
+			// Done with the parent Ethernet controller object so we release it.
+			(void)IOObjectRelease(controllerService);
+		}
+
+		// Done with the Ethernet interface object so we release it.
+		(void)IOObjectRelease(intfService);
+	}
+
+	return kernResult;
+}
+
 
 #else    // WIN_VERSION
 #include <strsafe.h>
@@ -246,9 +379,10 @@ public:
 	outlet<> out_platform {this, "(symbol) platform"};
 	outlet<> out_arch {this, "(symbol) architecture"};
 	outlet<> out_os {this, "(symbol) operating system version"};
+	outlet<> out_macaddr {this, "(symbol) primary MAC address"};
 
 
-	const char* osversionstring(void) {
+	const char* osversionstring() {
 		static char version[512];
 #ifdef MAC_VERSION
 		SInt32         versionMajor = 0, versionMinor = 0, versionBugFix = 0;
@@ -270,20 +404,50 @@ public:
 	}
 
 
+	const char* macaddr() {
+		static char macaddrstr[64];
+#ifdef MAC_VERSION
+		kern_return_t kernResult = KERN_SUCCESS;
+		io_iterator_t intfIterator;
+		UInt8         MACAddress[kIOEthernetAddressSize];
+
+		kernResult = FindEthernetInterfaces(&intfIterator);
+		if (KERN_SUCCESS != kernResult) {
+			c74::max::object_post(nullptr, "FindEthernetInterfaces returned 0x%08x\n", kernResult);
+		}
+		else {
+			kernResult = GetMACAddress(intfIterator, MACAddress, sizeof(MACAddress));
+			if (KERN_SUCCESS == kernResult) {
+				snprintf(macaddrstr, 64, "%02x:%02x:%02x:%02x:%02x:%02x", MACAddress[0], MACAddress[1], MACAddress[2], MACAddress[3],
+					MACAddress[4], MACAddress[5]);
+			}
+			else
+				macaddrstr[0] = 0;
+		}
+		IOObjectRelease(intfIterator);    // Release the iterator.
+#endif
+#ifdef WIN_VERSION
+
+#endif
+		return macaddrstr;
+	}
+
+
 	message<> bang {this, "bang", "Get info about the current max environment.",
 		MIN_FUNCTION {
+			out_macaddr.send(macaddr());
 			out_os.send(osversionstring());
 
 #ifdef C74_X64
 			out_arch.send("x86_64");
 #else    // 32-bit
-	out_arch.send("i386");
+			out_arch.send("i386");
 #endif
 
 #ifdef MAC_VERSION
 			out_platform.send("mac");
 #else    // WIN_VERSION
-	out_platform.send("win");
+			out_platform.send("win");
 #endif
 			return {};
 		}};
