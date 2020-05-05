adcac4de78b2efb899b423f449c27c03941a6fc3
Timothy Place <tim@cycling74.com>
2020-05-05 08:43:22 -0500

    min.buffer.index~: use consistent style for member naming.

--- source/projects/min.buffer.index_tilde/min.buffer.index_tilde.cpp
+++ source/projects/min.buffer.index_tilde/min.buffer.index_tilde.cpp
@@ -15,39 +15,39 @@ public:
     MIN_AUTHOR		{ "Cycling '74" };
     MIN_RELATED		{ "index~, buffer~, wave~" };
 
-    inlet<>  index_inlet	{ this, "(signal) Sample index" };
-    inlet<>  channel_inlet	{ this, "(float) Audio channel to use from buffer~" };
-    outlet<> output			{ this, "(signal) Sample value at index", "signal" };
-    outlet<> changed		{ this, "(symbol) Notification that the content of the buffer~ changed." };
+    inlet<>  m_inlet_index	    { this, "(signal) Sample index" };
+    inlet<>  m_inlet_channel	{ this, "(float) Audio channel to use from buffer~" };
+    outlet<> m_outlet_main		{ this, "(signal) Sample value at index", "signal" };
+    outlet<> m_outlet_changed	{ this, "(symbol) Notification that the content of the buffer~ changed." };
 
-    buffer_reference buffer { this,
+    buffer_reference m_buffer { this,
         MIN_FUNCTION {    // will receive a symbol arg indicating 'binding', 'unbinding', or 'modified'
-            changed.send(args);
+            m_outlet_changed.send(args);
             return {};
         }
     };
 
-    argument<symbol> name_arg {this, "buffer-name", "Initial buffer~ from which to read.",
+    argument<symbol> m_name_arg {this, "buffer-name", "Initial buffer~ from which to read.",
         MIN_ARGUMENT_FUNCTION {
-            buffer.set(arg);
+            m_buffer.set(arg);
         }
     };
 
-    argument<int> channel_arg {this, "channel", "Initial channel to read from the buffer~.",
+    argument<int> m_channel_arg {this, "channel", "Initial channel to read from the buffer~.",
         MIN_ARGUMENT_FUNCTION {
-            channel = arg;
+            m_channel = arg;
         }
     };
 
     message<> m_number { this, "number", "Set the channel to read from the buffer~. The channel number uses 1-based counting.",
         MIN_FUNCTION {
             if (inlet == 1)
-                channel = args[0];
-            return {}
+                m_channel = args[0];
+            return {};
         }
-    }
+    };
 
-    attribute<int, threadsafe::no, limit::clamp> channel {this, "channel", 1,
+    attribute<int, threadsafe::no, limit::clamp> m_channel {this, "channel", 1,
         description {"Channel to read from the buffer~. The channel number uses 1-based counting."},
         range {1, buffer_reference::k_max_channels}
     };
@@ -55,8 +55,8 @@ public:
     void operator()(audio_bundle input, audio_bundle output) {
         auto          in  = input.samples(0);                                     // get vector for channel 0 (first channel)
         auto          out = output.samples(0);                                    // get vector for channel 0 (first channel)
-        buffer_lock<> b(buffer);                                                  // gain access to the buffer~ content
-        auto          chan = std::min<size_t>(channel - 1, b.channel_count());    // convert from 1-based indexing to 0-based
+        buffer_lock<> b(m_buffer);                                                  // gain access to the buffer~ content
+        auto          chan = std::min<size_t>(m_channel - 1, b.channel_count());    // convert from 1-based indexing to 0-based
 
         if (b.valid()) {
             for (auto i = 0; i < input.frame_count(); ++i) {
