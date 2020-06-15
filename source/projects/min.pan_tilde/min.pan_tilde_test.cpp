8b4d654b8e225013af6a3088b9230bab53a97948
Timothy Place <tim@cycling74.com>
2020-06-15 14:57:21 -0500

    min.pan~ and min.xfade~ examples have thier names properly namespaced

--- source/projects/min.pan_tilde/min.pan_tilde_test.cpp
+++ source/projects/min.pan_tilde/min.pan_tilde_test.cpp
@@ -4,7 +4,7 @@
 ///	@license	Use of this source code is governed by the MIT License found in the License.md file.
 
 #include "c74_min_unittest.h"   // required unit test header
-#include "pan_tilde.cpp"        // need the source of our object so that we can access it
+#include "min.pan_tilde.cpp"        // need the source of our object so that we can access it
 
 // Unit tests are written using the Catch framework as described at
 // https://github.com/philsquared/Catch/blob/master/docs/tutorial.md
