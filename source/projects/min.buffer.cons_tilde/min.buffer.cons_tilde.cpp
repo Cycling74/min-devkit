6ebdb49367158549d16f80edfe679c5906e04bfb
Alex Norman <xnor@cycling74.com>
2020-08-21 15:12:44 -0700

    notes

--- source/projects/min.buffer.cons_tilde/min.buffer.cons_tilde.cpp
+++ source/projects/min.buffer.cons_tilde/min.buffer.cons_tilde.cpp
@@ -2,6 +2,9 @@
 
 using namespace c74::min;
 
+//#define DUMB
+//#define CRASH_ORDER
+
 class buffer_cons : public object<buffer_cons>, public vector_operator<> {
 public:
     MIN_DESCRIPTION	{ "buffer~ testing" };
@@ -9,18 +12,15 @@ public:
     MIN_AUTHOR		{ "Cycling '74" };
     MIN_RELATED		{ "index~, buffer~, wave~" };
 
-//    std::vector<std::unique_ptr<buffer_reference>> buffers;
-    std::vector<buffer_reference*> buffers;
 
+#ifdef DUMB
     buffer_cons() {
       create();
     }
 
     ~buffer_cons() {
         delete buffers[0];
-    }
-
-    void operator()(audio_bundle input, audio_bundle output) {
+        delete buffers[1];
     }
 
     message<> m_notify_meth { this, "notify",
@@ -28,18 +28,53 @@ public:
             notification n { args };
             atoms return_value { 0 };
 
-            for (auto& b : buffers)
-                return_value = b->handle_notification(this, args);
+            //for (auto& b : buffers)
+                //return_value = b->handle_notification(this, args);
             return return_value;
        }
     };
+    std::vector<buffer_reference*> buffers;
+#else
+
+    //if you define CRASH_ORDER, it looks like the notify method gets deleted
+    //and then the buffers get deleted, but deleteing the buffers calls the notify method
+#ifdef CRASH_ORDER
+    std::vector<std::unique_ptr<buffer_reference>> buffers;
+    std::unique_ptr<message<>> m_notify_meth;
+#else
+    std::unique_ptr<message<>> m_notify_meth;
+    std::vector<std::unique_ptr<buffer_reference>> buffers;
+#endif
+
+    buffer_cons() {
+      m_notify_meth = std::make_unique<message<>>(this, "notify",
+        MIN_FUNCTION {
+            notification n { args };
+            atoms return_value { 0 };
+            //for (auto& b : buffers)
+                //return_value = b->handle_notification(this, args);
+            return return_value;
+       });
+      create();
+    }
+    ~buffer_cons() {
+        //buffers.clear();
+        //m_notify_meth.reset();
+    }
+#endif
+
+    void operator()(audio_bundle input, audio_bundle output) {
+    }
 
 private:
     void create() {
-        for (int i = 0; i < 1; i++) {
-//          buffers.emplace_back(std::make_unique<buffer_reference>(this, nullptr, false));
-            buffers.push_back(new buffer_reference(this, nullptr, false));
-            buffers[i]->set(symbol("test" + std::to_string(i + 1)));
+        for (int i = 0; i < 2; i++) {
+#ifdef DUMB
+          buffers.emplace_back(new buffer_reference(this, nullptr, false));
+#else
+          buffers.emplace_back(std::make_unique<buffer_reference>(this, nullptr, false));
+#endif
+          buffers[i]->set(symbol("test" + std::to_string(i + 1)));
         }
     }
 };
