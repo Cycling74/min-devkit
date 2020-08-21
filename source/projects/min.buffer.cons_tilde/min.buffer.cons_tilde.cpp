4d312ec60aa15383b355823a3f6108daa44c5fda
Timothy Place <tim@cycling74.com>
2020-08-21 13:46:53 -0500

    min.buffer.cons~: experiments

--- source/projects/min.buffer.cons_tilde/min.buffer.cons_tilde.cpp
+++ source/projects/min.buffer.cons_tilde/min.buffer.cons_tilde.cpp
@@ -9,24 +9,37 @@ public:
     MIN_AUTHOR		{ "Cycling '74" };
     MIN_RELATED		{ "index~, buffer~, wave~" };
 
-    std::vector<std::unique_ptr<buffer_reference>> buffers;
-
-    virtual ~buffer_cons() {
-      buffers.clear();
-    }
+//    std::vector<std::unique_ptr<buffer_reference>> buffers;
+    std::vector<buffer_reference*> buffers;
 
     buffer_cons() {
       create();
     }
 
+    ~buffer_cons() {
+        delete buffers[0];
+    }
+
     void operator()(audio_bundle input, audio_bundle output) {
     }
 
+    message<> m_notify_meth { this, "notify",
+        MIN_FUNCTION {
+            notification n { args };
+            atoms return_value { 0 };
+
+            for (auto& b : buffers)
+                return_value = b->handle_notification(this, args);
+            return return_value;
+       }
+    };
+
 private:
     void create() {
-        for (int i = 0; i < 2; i++) {
-          buffers.emplace_back(std::make_unique<buffer_reference>(this));
-          buffers[i]->set(symbol("test" + std::to_string(i + 1)));
+        for (int i = 0; i < 1; i++) {
+//          buffers.emplace_back(std::make_unique<buffer_reference>(this, nullptr, false));
+            buffers.push_back(new buffer_reference(this, nullptr, false));
+            buffers[i]->set(symbol("test" + std::to_string(i + 1)));
         }
     }
 };
